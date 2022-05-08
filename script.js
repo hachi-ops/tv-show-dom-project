949afe8b6686d391b3b3f7ca2dba46278655d55c
hachi-ops <elen20@protonmail.com>
2022-05-09 00:01:17 +0100

    level-100

--- script.js
+++ script.js
@@ -5,8 +5,41 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const episodeContainer = document.getElementById("episode-list");
+
+  function formatSeriesAndEpisode(season, number) {
+    function padTheNumber(num) {
+      return num.toString().padStart(2, "0");
+    }
+    return `S${padTheNumber(season)}E${padTheNumber(number)}`;
+  }
+
+  episodeList.forEach((e) => {
+    const episode = document.createElement("div");
+    const heading = document.createElement("h2");
+    const summary = document.createElement("p");
+    const image = document.createElement("img");
+
+    image.src = e.image.medium;
+    image.alt = e.name;
+    image.setAttribute = ("id", "episode-img");
+
+    heading.innerText = `${e.name} - ${formatSeriesAndEpisode(
+      e.season,
+      e.number
+    )}`;
+
+    summary.innerHTML = e.summary;
+
+    episode.className = "episode";
+
+    episode.appendChild(heading);
+    episode.appendChild(image);
+    episode.appendChild(summary);
+    episodeContainer.appendChild(episode);
+
+    summary.className = "summary";
+  });
 }
 
 window.onload = setup;
