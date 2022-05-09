c4651f304572870e794b668cdc87d3b745e363a6
hachi-ops <elen20@protonmail.com>
2022-05-09 20:15:22 +0100

    level 500

--- script.js
+++ script.js
@@ -2,28 +2,23 @@ const searchBox = document.getElementById("search-box");
 const searchCount = document.getElementById("search-count");
 const selectMenu = document.getElementById("select-input");
 const showSelect = document.getElementById("select-show");
+const showList = document.getElementById("show-list");
+const resetPage = document.getElementById("reset-page");
 
 let currentEpisodes = [];
+
+searchBox.addEventListener("keyup", onKeyUp);
+selectMenu.addEventListener("change", onChange);
+showSelect.addEventListener("change", onChangeShow);
 
 function setup() {
   const allShows = getAllShows();
   makeSelectMenuForShows(allShows);
-
-  sendRequest(82).then((data) => {
-    currentEpisodes = data;
-    makePageForEpisodes(currentEpisodes);
-    makeSelectMenuForEpisodes(currentEpisodes);
-  });
-  searchBox.addEventListener("keyup", onKeyUp);
-  selectMenu.addEventListener("change", onChange);
-  showSelect.addEventListener("change", onChangeShow);
+  makePageForShows(allShows);
 }
 
 function makeSelectMenuForShows(shows) {
   shows.sort((showA, showB) => {
-    //in this case a and b are the show objects
-    //this gives us two variables, nameA and nameB which have been
-    //taken out of the show objects using destructuring
     const { name: nameA } = showA;
     const { name: nameB } = showB;
 
@@ -71,22 +66,22 @@ function formatSeriesAndEpisode(season, number) {
 
 function makePageForEpisodes(episodeList) {
   const episodeContainer = document.getElementById("episode-list");
-  episodeContainer.innerHTML = ""; //to empty episode container after each search
+  episodeContainer.innerHTML = "";
 
   episodeList.forEach((e) => {
     const episode = document.createElement("div");
+    const image = document.createElement("img");
     const heading = document.createElement("h3");
     const summary = document.createElement("p");
-    const image = document.createElement("img");
-
-    image.src = e.image.original;
-    image.alt = e.name;
-    image.setAttribute = ("id", "episode-img");
 
     heading.innerText = `${e.name} - ${formatSeriesAndEpisode(
       e.season,
       e.number
     )}`;
+
+    image.src = e.image.original;
+    image.alt = e.name;
+    image.setAttribute = ("id", "episode-img");
 
     summary.innerHTML = e.summary;
 
@@ -96,11 +91,49 @@ function makePageForEpisodes(episodeList) {
     episode.appendChild(image);
     episode.appendChild(summary);
     episodeContainer.appendChild(episode);
+
+    episode.addEventListener("click", () => {
+      console.log(e.url);
+    });
   });
+}
+
+function makePageForShows(shows) {
+  shows.forEach((show) => {
+    const showElement = document.createElement("div");
+    const heading = document.createElement("h3");
+    const image = document.createElement("img");
+    const summary = document.createElement("p");
+
+    heading.innerText = `${show.name} - Runtime:${show.runtime}`;
+    summary.innerHTML = show.summary;
+
+    image.src = show.image.original;
+    image.alt = show.name;
+    image.setAttribute = ("id", "show-img");
+    showElement.className = "episode";
+
+    showElement.appendChild(heading);
+    showElement.appendChild(image);
+    showElement.appendChild(summary);
+    showList.appendChild(showElement);
+
+    showElement.addEventListener("click", () => {
+      const showId = show.id;
+      sendRequest(showId).then((data) => {
+        currentEpisodes = data;
+        showList.style.display = "none";
+        makePageForEpisodes(currentEpisodes);
+        makeSelectMenuForEpisodes(currentEpisodes);
+      });
+    });
+  });
+  return shows;
 }
 
 function onKeyUp(event) {
   const searchTerm = event.target.value.toLowerCase();
+
   const filteredEpisodes = currentEpisodes.filter((e) => {
     const episodeName = e.name.toLowerCase();
     const episodeSummary = e.summary.toLowerCase();
@@ -108,31 +141,30 @@ function onKeyUp(event) {
       episodeName.includes(searchTerm) || episodeSummary.includes(searchTerm)
     );
   });
+
   const filteredCount = filteredEpisodes.length;
   const currentCount = currentEpisodes.length;
+
   const countString = `Displaying ${filteredCount} / ${currentCount}`;
+
   searchCount.innerText = countString;
   makePageForEpisodes(filteredEpisodes);
 }
 
 function onChange(event) {
   const episodeId = event.target.value;
-
   if (episodeId === "SHOW_ALL") {
     makePageForEpisodes(currentEpisodes);
   } else {
     const filteredEpisodes = currentEpisodes.filter((e) => {
-      // return e.id === +episodeId;
       return e.id === Number(episodeId);
     });
-
     makePageForEpisodes(filteredEpisodes);
   }
 }
 
 function onChangeShow(event) {
   const showId = event.target.value;
-  // console.log({ showId });
   sendRequest(showId).then((data) => {
     currentEpisodes = data;
     makePageForEpisodes(currentEpisodes);
@@ -141,9 +173,9 @@ function onChangeShow(event) {
 }
 
 function sendRequest(showId) {
-  const urlForRequest = `https://api.tvmaze.com/shows/${showId}/episodes`;
+  const urlForTheRequest = `https://api.tvmaze.com/shows/${showId}/episodes`;
 
-  return fetch(urlForRequest)
+  return fetch(urlForTheRequest)
     .then((res) => res.json())
     .then((data) => {
       return data;
